94aba72c42f000bcea4d8e63dbe350fb9126f347
Oscar <ogomezsts51l@gmail.com>
2020-11-12 01:16:53 +0100

    Done

--- nba-champions/src/app/app.component.ts
+++ nba-champions/src/app/app.component.ts
@@ -7,4 +7,91 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nba-champions';
+
+  team = [
+    {
+      year: "2015-16",
+      name: "Cleveland Cavaliers",
+      img: "assets/img/img1.png",
+      tag: "Best Players",
+      players: ["Lebron James", "Kyrio Irving", "Kevin Love", "J.R. Smith", "Anderson Varejao"]
+    },
+    {
+      year: "2014-15",
+      name: "Golden State Warriors",
+      img: "assets/img/img2.png",
+      tag: "Best Players",
+      players: ["Stephen Curry", "Klay Thompson", "Draymong Green", "Andrew Bogut", "Gestus Ezeli"]
+    },
+    {
+      year: "2013-14",
+      name: "San Antonio Spurs",
+      img: "assets/img/img3.png",
+      tag: "Best Players",
+      players: ["Tim Duncan", "Tony Parker", "Manu Ginóbili", "Kawhi Leonard", "Tiago Splitter"]
+    },
+    {
+      year: "2015-16",
+      name: "Cleveland Cavaliers",
+      img: "assets/img/img1.png",
+      tag: "Best Players",
+      players: ["Lebron James", "Kyrio Irving", "Kevin Love", "J.R. Smith", "Anderson Varejao"]
+    },
+    {
+      year: "2014-15",
+      name: "Golden State Warriors",
+      img: "assets/img/img2.png",
+      tag: "Best Players",
+      players: ["Stephen Curry", "Klay Thompson", "Draymong Green", "Andrew Bogut", "Gestus Ezeli"]
+    },
+    {
+      year: "2013-14",
+      name: "San Antonio Spurs",
+      img: "assets/img/img3.png",
+      tag: "Best Players",
+      players: ["Tim Duncan", "Tony Parker", "Manu Ginóbili", "Kawhi Leonard", "Tiago Splitter"]
+    },
+    {
+      year: "2015-16",
+      name: "Cleveland Cavaliers",
+      img: "assets/img/img1.png",
+      tag: "Best Players",
+      players: ["Lebron James", "Kyrio Irving", "Kevin Love", "J.R. Smith", "Anderson Varejao"]
+    },
+    {
+      year: "2014-15",
+      name: "Golden State Warriors",
+      img: "assets/img/img2.png",
+      tag: "Best Players",
+      players: ["Stephen Curry", "Klay Thompson", "Draymong Green", "Andrew Bogut", "Gestus Ezeli"]
+    },
+    {
+      year: "2013-14",
+      name: "San Antonio Spurs",
+      img: "assets/img/img3.png",
+      tag: "Best Players",
+      players: ["Tim Duncan", "Tony Parker", "Manu Ginóbili", "Kawhi Leonard", "Tiago Splitter"]
+    },
+    {
+      year: "2015-16",
+      name: "Cleveland Cavaliers",
+      img: "assets/img/img1.png",
+      tag: "Best Players",
+      players: ["Lebron James", "Kyrio Irving", "Kevin Love", "J.R. Smith", "Anderson Varejao"]
+    },
+    {
+      year: "2014-15",
+      name: "Golden State Warriors",
+      img: "assets/img/img2.png",
+      tag: "Best Players",
+      players: ["Stephen Curry", "Klay Thompson", "Draymong Green", "Andrew Bogut", "Gestus Ezeli"]
+    },
+    {
+      year: "2013-14",
+      name: "San Antonio Spurs",
+      img: "assets/img/img3.png",
+      tag: "Best Players",
+      players: ["Tim Duncan", "Tony Parker", "Manu Ginóbili", "Kawhi Leonard", "Tiago Splitter"]
+    },
+  ]
 }
